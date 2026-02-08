e7054d76274efd1d7f0658716eb7c9e4e8e10be4
oregen-cpu <oregen09@gmail.com>
2026-02-08 17:18:44 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+import http from "http";
+import fs from "fs";
+import path from "path";
+
+const PORT = 25005;
+
+http.createServer((req, res) => {
+  const file = req.url === "/" ? "main.html" : req.url.slice(1);
+  const filePath = path.join(process.cwd(), file);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("Fichier non trouvé");
+    } else {
+      res.writeHead(200);
+      res.end(data);
+    }
+  });
+}).listen(PORT, "0.0.0.0", () => {
+  console.log(`Serveur lancé sur http://0.0.0.0:${PORT}`);
+});
