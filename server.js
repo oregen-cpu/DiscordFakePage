9306d5124cae04d0e055d29919a3f066e5c5bdf0
oregen-cpu <oregen09@gmail.com>
2026-02-08 17:20:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import http from "http";
 import fs from "fs";
 import path from "path";
 
-const PORT = 403; #Put youre port here
+const PORT = 403; #Enter your port here
 
 http.createServer((req, res) => {
   const file = req.url === "/" ? "main.html" : req.url.slice(1);
@@ -21,4 +21,5 @@ http.createServer((req, res) => {
 }).listen(PORT, "0.0.0.0", () => {
   console.log(`Serveur lancé sur http://0.0.0.0:${PORT}`);
 });
+
 
